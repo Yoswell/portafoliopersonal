2be0f484822930ce68d6679b6468b72d2c5b50e0
Yoswell Badilla <113799193+Yoswell@users.noreply.github.com>
2024-12-21 22:32:26 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,58 +1,3 @@
-const var1 = document.getElementById('card-1');
-const var2 = document.getElementById('card-2');
-const var3 = document.getElementById('card-3');
-const var4 = document.getElementById('card-4');
-const var5 = document.getElementById('card-5');
-const var6 = document.getElementById('card-6');
-
-document.getElementById('but-1').addEventListener('click', (e) => {
-    e.preventDefault();
-    styles(var2, var3, var4, var5, var6);
-    styles_flex(var1);
-});
-
-document.getElementById('but-2').addEventListener('click', (e) => {
-    e.preventDefault();
-    styles(var1, var3, var4, var5, var6);
-    styles_flex(var2);
-});
-
-document.getElementById('but-3').addEventListener('click', (e) => {
-    e.preventDefault();
-    styles(var1, var2, var4, var5, var6);
-    styles_flex(var3);
-});
-
-document.getElementById('but-4').addEventListener('click', (e) => {
-    e.preventDefault();
-    styles(var1, var2, var3, var5, var6);
-    styles_flex(var4);
-});
-
-document.getElementById('but-5').addEventListener('click', (e) => {
-    e.preventDefault();
-    styles(var1, var2, var3, var4, var6);
-    styles_flex(var5);
-});
-
-document.getElementById('but-6').addEventListener('click', (e) => {
-    e.preventDefault();
-    styles(var1, var2, var3, var4, var5);
-    styles_flex(var6);
-});
-
-function styles(...items) {
-    items.forEach(x => {
-        x.style.display = 'none';
-        x.style.transform = 'translateX(-2000%)';
-    });
-}
-
-function styles_flex(x) {
-    x.style.display = 'flex';
-    x.style.transform = 'translateX(0%)';
-}
-
 const colors_dark = {
     '--color-bg': '#0a0a0b',
     '--color-bg-alt': '#111112',
@@ -60,7 +5,8 @@ const colors_dark = {
     '--color-primary': '#ff4264',
     '--color-border': '#171718',
     '--color-shadow': '#202020',
-    '--color-border-two': '#303030',
+    '--color-border-two': '#171718',
+    '--color-border-tree': '#252525',
     '--color-shadow-inset': '#41262c',
     '--color-font-primary': '#fff',
     '--color-font-secondary': '#a3a3a3',
@@ -75,11 +21,12 @@ const colors_light = {
     '--color-primary': '#ff4264',
     '--color-border': '#f4f4f9',
     '--color-shadow': '#e4e4e9',
-    '--color-border-two': '#bbbbbb',
+    '--color-border-two': '#e4e4e9',
+    '--color-border-tree': '#dddddd',
     '--color-shadow-inset': '#ffaabc',
     '--color-font-primary': '#000',
     '--color-font-secondary': '#555',
-    '--color-accent': '#86b0ff',
+    '--color-accent': '#7b97d1',
     '--color-scrollbar': '#ccc'
 }
 
